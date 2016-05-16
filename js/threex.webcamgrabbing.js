c8c1611af3a1dfb3c3b16f59ae6cb79fa5942777
spacejuice <dani_schaer@bluewin.ch>
2016-05-16 17:04:08 +0200

    Add files via upload

--- js/threex.webcamgrabbing.js
+++ js/threex.webcamgrabbing.js
@@ -21,11 +21,11 @@ THREEx.WebcamGrabbing = function(){
 	domElement.style.zIndex = -1;
         domElement.style.position = 'absolute'
 
-	domElement.style.top = '50%'
-	domElement.style.left = '50%'
-	domElement.style.marginRight = '50%'
-	domElement.style.transform = 'translate(-50%, -50%)'
-	domElement.style.minWidth = '100%'
+	//domElement.style.top = '50%'
+	//domElement.style.left = '50%'
+	//domElement.style.marginRight = '50%'
+	//domElement.style.transform = 'translate(-50%, -50%)'
+	//domElement.style.minWidth = '100%'
 
 	domElement.style.top = '0px'
 	domElement.style.left = '0px'
